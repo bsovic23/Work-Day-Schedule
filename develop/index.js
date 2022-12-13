87251111f3856d6c16a551513bc84d22fefed572
Brit Sovic <bsovic@tulane.edu>
2022-12-13 11:41:50 -0500

    updating js

--- develop/index.js
+++ develop/index.js
@@ -1,6 +1,6 @@
 console.log('this js script works');
 
-// Create DOM element and function for the current date
+// Create DOM element and function for the current date - function should be immediately called to load to the screen
 
 
-// 
+// Create functions for taking in the notes for the timeblocks -> should each have id that puts them in the correct block
